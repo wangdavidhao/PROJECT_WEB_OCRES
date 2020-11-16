35a91a6e6185f91c9b7d7fbe8ca802679068c54d
wangdavidhao <wangdavid1999@gmail.com>
2020-11-16 19:31:34 +0100

    GetFranceGender OK

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -27,6 +27,8 @@ export const Dashboard = () => {
     const [countryHistoric, setCountryHistoric] = useState({});
     const [countriesHistoric, setCountriesHistoric] = useState([]);
 
+    const [gender, setGender] = useState([]);
+
     //First method
     /**
      * Fonction qui va get toute la data sur le monde 
@@ -135,9 +137,47 @@ export const Dashboard = () => {
     }
 
 
-    const fetchFranceData = async () => {
+    /**
+     * Fonction qui va récup un fichier CSV (COMMA SEPARATED VALUE) et retourner sous forme d'objet JS
+     */
+    const fetchFranceGenderData = async () => {
         const response = await axios.get(`${API_URL_FRANCE}/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7`);
-        console.log(response.data);
+        const data = await response.data;
+
+        const franceGenderData = [];
+
+        //Transforme les lignes CSV (Comma Separated Value) => Split à chaque retour à la ligne - On prend la première ligne (header) 
+        //On split le header à chaque ';' => Renvoie un Array avec tous les labels du header
+
+        const splitedCsv = data.toString().split('\n');
+        const csvTitle = splitedCsv[0];
+        const csvTitleArray = csvTitle.split(';');
+
+        const csvTitleArrayFinal = [];
+        for(let i=0 ; i < csvTitleArray.length ; i++){
+            const title = csvTitleArray[i].substring(1, csvTitleArray[i].length-1);
+            csvTitleArrayFinal.push(title);
+        }
+
+        for(let i=1; i< splitedCsv.length ; i++){
+
+            const csvData = splitedCsv[i];
+            const csvDataArray = csvData.split(';');
+
+            const csvDataArrayTemp = [];
+            const obj = {};
+            for(let j=0 ; j< csvDataArray.length ; j++){
+                const data = csvDataArray[j].substring(1, csvDataArray[j].length-1);
+                csvDataArrayTemp.push(data);
+                
+            }   
+            csvTitleArrayFinal.forEach((title, data) => {
+                obj[title] = csvDataArrayTemp [data];
+            });
+            franceGenderData.push(obj);
+        }
+
+        setGender(franceGenderData);
     }
 
     useEffect(() => {
@@ -145,7 +185,7 @@ export const Dashboard = () => {
         // fetchCountriesData('france');
         // fetchContinentsData('south america');
         // fetchCountriesHistoric('france');
-        fetchFranceData();
+        //fetchFranceGenderData();
     }, []);
 
     return (
