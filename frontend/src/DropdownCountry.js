904d599c6a61c8d04c3670f7d65c627bc51cdc6c
wangdavidhao <wangdavid1999@gmail.com>
2020-11-21 00:24:18 +0100

    Dropdown

--- frontend/src/DropdownCountry.js
+++ frontend/src/DropdownCountry.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import {Container, Row, Col, Dropdown} from 'react-bootstrap';
-
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 
 const DropdownCountry = ({countries}) => {
-
     return (
         <Container>
             <Row>
                 <Col>
-                    <Dropdown  >
-                        <Dropdown.Toggle variant="secondary" id="dropdown-basic"  >
-                            
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            {countries.map((country) => (
-                                <Dropdown.Item>{country.country}</Dropdown.Item>
-                            ))}
-                        </Dropdown.Menu>
-                    </Dropdown>
+                    <FormControl>
+                        <Select
+                        variant="outlined"
+                        >
+                        <MenuItem value="world">World</MenuItem>
+                        {countries.map((country) => (
+                            <MenuItem value={country.countryInfo.iso2}>{country.country}</MenuItem>
+                        ))}
+                        </Select>
+                    </FormControl>
                 </Col>
             </Row>
         </Container>
