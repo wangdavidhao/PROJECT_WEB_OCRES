3fa1e5394151641b8abcd989ecc93797a4ae1b65
BenjaminSadoun <benji.sadoun@gmail.com>
2020-11-24 23:59:14 +0100

    recup données par mois

--- frontend/src/widgets/CircularGraph.js
+++ frontend/src/widgets/CircularGraph.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 import './CircularGraph.css';
+import {hexToHSL, parseHSL, harmonize} from './util.js' ;
 
 import PropTypes from 'prop-types';
 
@@ -10,71 +11,6 @@ let data = [
     { name: 'Femmes', value: 35, },
   ];
 
-function hexToHSL(H) {
-    // Convert hex to RGB first
-    let r = 0, g = 0, b = 0;
-    if (H.length === 4) {
-      r = "0x" + H[1] + H[1];
-      g = "0x" + H[2] + H[2];
-      b = "0x" + H[3] + H[3];
-    } else if (H.length === 7) {
-      r = "0x" + H[1] + H[2];
-      g = "0x" + H[3] + H[4];
-      b = "0x" + H[5] + H[6];
-    }
-    // Then to HSL
-    r /= 255;
-    g /= 255;
-    b /= 255;
-    let cmin = Math.min(r,g,b),
-        cmax = Math.max(r,g,b),
-        delta = cmax - cmin,
-        h = 0,
-        s = 0,
-        l = 0;
-  
-    if (delta === 0)
-      h = 0;
-    else if (cmax === r)
-      h = ((g - b) / delta) % 6;
-    else if (cmax === g)
-      h = (b - r) / delta + 2;
-    else
-      h = (r - g) / delta + 4;
-  
-    h = Math.round(h * 60);
-  
-    if (h < 0)
-      h += 360;
-  
-    l = (cmax + cmin) / 2;
-    s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
-  
-    return "hsl(" + h + "," + s + "%," + l + "%)";
-}
-
-function parseHSL(str) {
-    var hsl, h, s, l
-    hsl = str.replace(/[^\d,]/g, '').split(',')   // strip non digits ('%')  
-    h = Number(hsl[0])                            // convert to number
-    s = Number(hsl[1])
-    l = Number(hsl[2])
-    return [h, s, l]                              // return parts
-}
-
-function harmonize(color, start=30, end=90, interval=10) {
-    const colors = [color]
-    const [h, s, l] = parseHSL(color)
-
-    for(let i = start; i <= end; i += interval) {
-        const h1 = (h + i) % 360;
-        const c1 = `hsl(${h1}, ${s}%, ${l}%)`;
-        colors.push(c1);
-    }
-    return colors
-}
 
 const renderActiveShape = (props) => {
     const RADIAN = Math.PI / 180;
