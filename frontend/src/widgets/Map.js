47f6b71b3cdf0d10016343c9dac0c556a05f665f
wangdavidhao <wangdavid1999@gmail.com>
2020-11-24 17:08:56 +0100

    create map component

---
+++ frontend/src/widgets/Map.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Map.css';
+import {Map as WorldMap, TileLayer} from 'react-leaflet';
+
+const Map = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Map;
