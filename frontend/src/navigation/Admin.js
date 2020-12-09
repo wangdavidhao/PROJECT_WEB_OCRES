fbe7eac8967e615623a0885028e7bdeb387c5fc0
wangdavidhao <wangdavid1999@gmail.com>
2020-12-09 22:55:21 +0100

    Page connexion admin

--- frontend/src/navigation/Admin.js
+++ frontend/src/navigation/Admin.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import {ListData} from './../widgets/ListData.js'
+
+
+import './Admin.css';
 
 
 //Local imports
 import Navbar from './Navbar.js';
 
 const Admin = () => {
+
+    const [isLogged, setIsLogged] = useState(true);
+
+    const handleSignIn = (e) => {
+        e.prevent.default();
+    };
+
     return (
         <Container fluid={true} className="admin">
             <Navbar page="admin"/>
             <Row>
                 <Col>
-                    <h1>Page Admin</h1>
-                    <ListData isAdmin={true}/>
+                    
+                    {isLogged ? <SignIn handleSignIn={handleSignIn}/> : <ListData isAdmin={true}/> }
                 </Col>
             </Row>
         </Container>
     )
 }
 
+const SignIn = ({handleSignIn}) => {
+
+    return(
+        <div>
+            <form className="admin__signIn" onSubmit={handleSignIn}>
+                <h1>Connexion</h1>
+                <label for="email">Email : </label>
+                <input type="email" name="email" placeholder="Mail"/>
+                <label for="password">Mot de passe : </label>
+                <input type="password" name="password" placeholder="Mdp"/>
+                <button type="submit">Connexion</button>
+            </form>
+        </div>
+    )
+    
+}
 
 export default Admin;
